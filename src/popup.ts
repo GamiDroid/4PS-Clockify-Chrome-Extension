fdb85da7e88252b5af295ee9ef2c84150f3d65f2
Kevin Damen <k.damen@bellt.nl>
2024-10-01 22:33:26 +0200

    fix typescript chrome-types intellisence

--- src/popup.ts
+++ src/popup.ts
@@ -1,2 +1,11 @@
+
+let storageKeyApiKey: string = "clockify-api-key";
+let apiKey = chrome.storage.local.get(storageKeyApiKey);
+if (apiKey == undefined) {
+    chrome.storage.local.set({ storageKeyApiKey: 1 });
+}
+
+console.log('clockify api key: ' + apiKey);
+
 let message: string = 'Hello world';
 console.log(message);
